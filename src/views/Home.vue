321d59cbbe9684e0ba6dd0286eee5c3c01f08376
hooman <hooman182@gmail.com>
2021-08-22 19:13:16 +0430

    Import TheContent component
    Create new elements in home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="home">
+    <main class="main">
+      <h1 class="view-title">find random quote's</h1>
+      <TheContent />
+      <button class="btn btn-primary">
+        <i class="fas fa-sync-alt"></i>
+        Find New Quote
+      </button>
+    </main>
   </div>
 </template>
 
 <script>
+import TheContent from "@/components/TheContent";
 export default {
   name: "Home",
+  components: { TheContent },
 };
 </script>
+
+<style lang="scss" scoped>
+.main {
+  @include flex(column, none, center);
+}
+</style>
