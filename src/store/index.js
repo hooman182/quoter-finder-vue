2e1102874b0410c1c0c2649f3d5af54e525d9b3b
hooman <hooman182@gmail.com>
2021-08-22 23:53:29 +0430

    Fetch data from api

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
+import axios from "axios";
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    data: ""
+  },
+  mutations: {
+    getData(state, response) {
+      state.data = response
+    }
+  },
+  actions: {
+    async fetchData({ commit }) {
+      await axios.get("https://api.quotable.io/random").then(res => commit('getData', res.data));
+    }
+  },
   modules: {},
 });
