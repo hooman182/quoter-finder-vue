7459fa46b3e40b2e0e622bc36093ee7423887dd8
hooman <hooman182@gmail.com>
2021-08-22 23:54:01 +0430

    Send data to content component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <main class="main">
       <h1 class="view-title">find random quote's</h1>
-      <TheContent />
-      <button class="btn btn-primary">
+      <TheContent :data="data" />
+      <button class="btn btn-primary" @click="getNewData">
         <i class="fas fa-sync-alt"></i>
         Find New Quote
       </button>
@@ -12,10 +12,25 @@
 </template>
 
 <script>
+import { useStore } from "vuex";
+import { computed } from "@vue/reactivity";
 import TheContent from "@/components/TheContent";
+import { onMounted } from "@vue/runtime-core";
 export default {
   name: "Home",
   components: { TheContent },
+  setup() {
+    const store = useStore();
+    const data = computed(() => store.state.data);
+    function getNewData() {
+      store.dispatch("fetchData");
+    }
+    onMounted(() => getNewData());
+    return {
+      data,
+      getNewData,
+    };
+  },
 };
 </script>
 
