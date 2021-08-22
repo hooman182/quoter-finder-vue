50c738cef2a941f6b4b579439277d868ff166f8e
hooman <hooman182@gmail.com>
2021-08-22 19:09:45 +0430

    Import TheNavbar component to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
+<script>
+import Navbar from "@/components/TheNavbar";
+export default {
+  name: "App",
+  components: {Navbar}
+}
+</script>
 
 <style lang="scss">
 
