ab63c7c2795c998f92389dbf8880a17bb41d39fc
hooman <hooman182@gmail.com>
2021-08-22 19:08:33 +0430

    feat: Create new component
    Create TheNavbar component

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar-lists">
+      <li class="navbar-list">
+        <router-link to="/" class="navbar-list__link">Find quote</router-link>
+      </li>
+      <li class="navbar-list">
+        <router-link to="/#" class="navbar-list__link">Bookmarks</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  width: 100%;
+  padding: 0.75em;
+  background: $light;
+  display: flex;
+  justify-content: space-between;
+  #{&}-lists {
+    list-style: none;
+    display: flex;
+  }
+  #{&}-list {
+    &__link {
+      padding: 0.75em 1em;
+      color: $white;
+      font-size: 1rem;
+      font-weight: 500;
+    }
+  }
+}
+</style>
