7baa93c788a41df0e4f387d73e5dcd6d59ac4fdf
hooman <hooman182@gmail.com>
2021-08-23 02:31:01 +0430

    feat: Create new components
    Create HistorySection component
    
    Create HistoryItem component
    
    Import HistoryItem component to HistorySection

---
+++ src/components/HistorySection/HistoryItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="item">
+    <p class="item-text">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas iure
+      dolor quos debitis ab? Maxime nostrum quaerat, repellendus fuga ab magni
+      facere pariatur voluptate voluptas eius omnis illum sapiente odit.
+    </p>
+    <div class="item-footer">
+      <span class="item-footer__title">Bill Gates</span>
+      <div>
+        <button class="item-footer__btn">
+          <i class="fas fa-clone"></i>
+        </button>
+        <button class="item-footer__btn">
+          <i class="far fa-bookmark"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.item {
+  background: $white;
+  border-radius: 6px;
+  overflow: hidden;
+  margin: 0.5em;
+  #{&}-text {
+    padding: 0.75em;
+    font-size: 0.9rem;
+    font-weight: 600;
+    color: $secondary;
+  }
+  #{&}-footer {
+    @include flex(row, space-between, center);
+    padding: 0.25em 0.75em;
+    background: $secondary;
+    font-size: 0.95rem;
+    color: $white;
+    &__title {
+      font-weight: 600;
+    }
+    &__btn {
+      background: none;
+      padding: 0.25em;
+      cursor: pointer;
+      color: $white;
+    }
+  }
+}
+</style>
---
+++ src/components/HistorySection/HistorySection.vue
@@ -0,0 +1,33 @@
+<template>
+<!-- v-for="(data, index) in dataHistory" :key="index" -->
+  <section class="section">
+    <HistoryItem  />
+    <button class="btn btn-danger">
+      <i class="fas fa-trash"></i>
+      Clear History
+    </button>
+  </section>
+</template>
+
+<script>
+import { computed } from "@vue/reactivity";
+import { useStore } from "vuex";
+import HistoryItem from "./HistoryItem";
+export default {
+  name: "HistorySection",
+  components: { HistoryItem },
+  setup() {
+    const store = useStore();
+    const dataHistory = computed(() => store.state.dataHistory);
+    return { dataHistory };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.section {
+  width: 50%;
+  margin: 2em 0 1em 0;
+  @include flex(column, none, center);
+}
+</style>
