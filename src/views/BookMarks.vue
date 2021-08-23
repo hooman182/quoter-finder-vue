6fbc5ce0b2a28a65c1373d5c210b991de9b63022
hooman <hooman182@gmail.com>
2021-08-23 16:23:24 +0430

    feat: Create bookmark view
    Import HistoryItem component

---
+++ src/views/BookMarks.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="section">
+    <HistoryItem v-for="data,index in boomarkedData" :key="index" :data="data"/>
+  </section>
+</template>
+
+<script>
+import HistoryItem from "@/components/HistorySection/HistoryItem";
+import { useStore } from "vuex";
+import { computed } from "@vue/reactivity";
+export default {
+  name: "BookMarks",
+    components: { HistoryItem },
+  setup() {
+    const store = useStore();
+    const boomarkedData = computed(() => store.getters.boomarkedData);
+    return { boomarkedData };
+  },
+};
+</script>
+
+<style>
+.section {
+  width: 50%;
+  margin: 2em auto;
+}
+</style>
