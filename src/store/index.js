3aa2b8f749f80e7724bfbbbb7cb71edd0d59fcaa
hooman <hooman182@gmail.com>
2021-08-23 15:30:07 +0430

    feat: Define new functions and variables in store

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,30 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    data: ""
+    data: "",
+    dataHistory: [],
   },
   mutations: {
     getData(state, response) {
-      state.data = response
+      response.bookmark = false
+      state.data = response;
+      state.dataHistory = [...state.dataHistory, response];
+    },
+    bookmarkItem(state, index) {
+      state.dataHistory[index].bookmark = !state.dataHistory[index].bookmark
+    },
+    removeBookmark(state, item) {
+      state.bookmarks.splice(item, 1);
+    },
+    clearHistory(state) {
+      state.dataHistory = [];
     }
   },
   actions: {
     async fetchData({ commit }) {
-      await axios.get("https://api.quotable.io/random").then(res => commit('getData', res.data));
+      await axios.get("https://api.quotable.io/random")
+        .then(res => commit('getData', res.data))
+        .catch(res => console.log(res));
     }
   },
   modules: {},
